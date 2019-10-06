abd83c42724a62ada6b45946dffa67bdae5e687c
agga1 <agud100@gmail.com>
2019-10-07 01:36:10 +0200

    boxes loading

--- src/popup/main.js
+++ src/popup/main.js
@@ -1,19 +1,36 @@
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    var activeTab = tabs[0]
+    chrome.tabs.sendMessage(activeTab.id, { message: "load settings" })
+})
+let settings = { mouse: true, links: false }
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'settings') {
+        console.log(request.message)
+        // settings =
+        mouseTrk.checked = settings.mouse
+        urlTrk.checked = settings.links
+    }
+})
+
+const mouseTrk = document.querySelector("#mouse_tracking>input")
+const urlTrk = document.querySelector("#url_tracking>input")
+
 document.getElementById("uncheck_boxes").addEventListener("click", e => {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { "message": "uncheck all" });
-    });
+        var activeTab = tabs[0]
+        chrome.tabs.sendMessage(activeTab.id, { "message": "uncheck all" })
+    })
 })
 document.getElementById("mouse_tracking").addEventListener("click", e => {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { "message": "prevent mouse tracking" });
-    });
+        var activeTab = tabs[0]
+        chrome.tabs.sendMessage(activeTab.id, { "message": "prevent mouse tracking" })
+    })
 })
 document.getElementById("url_tracking").addEventListener("click", e => {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { "message": "prevent url tracking" });
-    });
+        var activeTab = tabs[0]
+        chrome.tabs.sendMessage(activeTab.id, { "message": "prevent url tracking" })
+    })
 })
 
