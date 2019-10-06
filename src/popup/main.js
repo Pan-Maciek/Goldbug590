64570627bc5e982ad778888b4e3783d32fa89259
agga1 <agud100@gmail.com>
2019-10-07 00:59:42 +0200

    boxes send messages

--- src/popup/main.js
+++ src/popup/main.js
@@ -1,7 +1,19 @@
-document.getElementById("uncheckBoxes").addEventListener("click", e => {
+document.getElementById("uncheck_boxes").addEventListener("click", e => {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         var activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, { "message": "uncheck all" });
+    });
+})
+document.getElementById("mouse_tracking").addEventListener("click", e => {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { "message": "prevent mouse tracking" });
+    });
+})
+document.getElementById("url_tracking").addEventListener("click", e => {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { "message": "prevent url tracking" });
     });
 })
 
