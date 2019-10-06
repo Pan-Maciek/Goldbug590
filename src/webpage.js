991817d7a9cbe0666c1d79605f6f09ea88d817a3
agga1 <agud100@gmail.com>
2019-10-06 20:39:17 +0200

    logo

--- src/webpage.js
+++ src/webpage.js
@@ -1,14 +1,16 @@
-!function () {
-    function mockEventObject() {
+!(function() {
+  fetch("http://192.168.20.35:5000/")
+    .then(res => res.text())
+    .then(console.log);
 
-    }
+  function mockEventObject() {}
 
-    const EventTarget = window.__proto__.__proto__.__proto__
-    const addEventListener = EventTarget.addEventListener
-    EventTarget.addEventListener = function (eventName, callback) {
-        addEventListener.call(this, eventName, e => {
-            const mockObject = mockEventObject(e)
-            callback(callback)
-        })
-    }
-}()
+  const EventTarget = window.__proto__.__proto__.__proto__;
+  const addEventListener = EventTarget.addEventListener;
+  EventTarget.addEventListener = function(eventName, callback) {
+    addEventListener.call(this, eventName, e => {
+      const mockObject = mockEventObject(e);
+      callback(callback);
+    });
+  };
+})();
