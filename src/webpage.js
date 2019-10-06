7766216e591f46e8f9873e02587a258b05133f3d
pan-maciek <kozieja.maciej@gmail.com>
2019-10-06 19:42:02 +0200

    mocking basics

--- src/webpage.js
+++ src/webpage.js
@@ -1,14 +1,25 @@
 !function () {
-    function mockEventObject() {
-
+    function propertyObserver(object, property, getCallback) {
+        if(!Object.getOwnPropertyDescriptor(object, property)) {
+            let val = object[property]
+            Object.defineProperty(object, property, {
+                get() {
+                    getCallback(val ,property)
+                    return val
+                }, 
+                set(value) { return val = value }
+            })
+        }
+        return object
     }
+
 
     const EventTarget = window.__proto__.__proto__.__proto__
     const addEventListener = EventTarget.addEventListener
     EventTarget.addEventListener = function (eventName, callback) {
         addEventListener.call(this, eventName, e => {
-            const mockObject = mockEventObject(e)
-            callback(callback)
+            const mockedObject = propertyObserver(e, 'srcElement', console.log)
+            callback(mockedObject)
         })
     }
 }()
