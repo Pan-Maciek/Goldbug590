3c97c698511f64cda87eb816082f59dfbc0c037b
pan-maciek <kozieja.maciej@gmail.com>
2019-10-06 23:05:16 +0200

    message passing

--- src/popup/main.js
+++ src/popup/main.js
@@ -1,3 +1,7 @@
 document.getElementById("uncheckBoxes").addEventListener("click", e => {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { "message": "uncheck all" });
+    });
 })
 
